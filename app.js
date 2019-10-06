08093de497050a1d85f5932c33070f30d236058e
Martuza Shimul <martuza.shimul@gmail.com>
2019-10-07 02:05:44 +0600

    final version 1.0

--- app.js
+++ app.js
@@ -28,5 +28,8 @@ window.addEventListener("load", () => {
     visual.appendChild(bubble);
     bubble.style.backgroundColor = colors[index];
     bubble.style.animation = "jump 1s ease";
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
   };
 });
